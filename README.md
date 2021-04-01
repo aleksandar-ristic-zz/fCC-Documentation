a17e73628c62482a1619d13749d998632c6c8784
Aleksandar Ristic <aristic665@gmail.com>
2021-04-01 16:19:35 +0200

    Finished JS and navbar

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Basic Documentation page
-
 ## Made with vanilla js, scss
 
 ## fully responsive
--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+/* *** Navbar *** */
+const navbar = document.getElementById('navbar');
+const navButton = document.querySelector('.navbar-button');
 
+navButton.addEventListener('click', function () {
+    navbar.classList.toggle('open');
 
+    if (navbar.classList.contains('open')) {
+        navButton.innerHTML = `<i class="fas fa-times"></i>`
+    }
+    else {
+        navButton.innerHTML = `<i class="far fa-caret-square-right"></i>`
+    }
+});
 
+/* *** Accordion *** */
+const navLink = document.querySelectorAll('.nav-link');
+const accordion = document.querySelectorAll('.nav-container');
+
+navLink.forEach((link, idx )=> link.addEventListener('click', function() {
+        accordion[idx].classList.toggle('open');
+}));
